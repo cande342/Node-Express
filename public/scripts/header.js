e9ad197563f71ce25d167f7574fdf5f11601e080
candela <gcandela894@gmail.com>
2024-07-05 13:29:06 -0300

    cssfinal

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const token = localStorage.getItem('token');
+    const loginButton = document.querySelector('nav.nav-items ul li:last-child a');
 
     if (token) {
         // Usuario autenticado: Cambiar el texto del botón a "Cerrar Sesión"
-        const loginButton = document.querySelector('nav.nav-items ul li:last-child a');
         if (loginButton) {
             loginButton.textContent = 'Cerrar Sesión';
             loginButton.href = '#'; 
@@ -14,10 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     } else {
         // Usuario no autenticado: Mantener el texto original del botón "Iniciar Sesión"
-        const loginButton = document.querySelector('nav.nav-items ul li:last-child a');
         if (loginButton) {
             loginButton.textContent = 'Iniciar Sesión';
             loginButton.href = 'login.html'; // Ruta al formulario de inicio de sesión
         }
     }
+
+    // Lógica del menú hamburguesa
+    const menuToggle = document.getElementById('menu-toggle');
+    const navItems = document.querySelector('.nav-items ul');
+
+    menuToggle.addEventListener('click', function() {
+        navItems.classList.toggle('show');
+    });
 });
